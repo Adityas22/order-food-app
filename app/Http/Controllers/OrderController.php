5114fab32b206f33d6dd16c1a04ddd1d2137a493
Adityas22 <adityasept2209@gmail.com>
2025-12-11 00:52:05 +0700

    create order detail

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item;
 use App\Models\Order;
+use App\Models\OrderDetail;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -19,14 +21,27 @@ class OrderController extends Controller
 
         $data['order_time'] = now()->toDateTimeString();
         $data['status'] = 'pending';
-        $data['total_price'] = 0;
+        $data['total_price'] = 15000;
 
         $data['waiter_id'] =  $request->user()->id;
         // $data['cashier_id'] = null;
+        $data['items'] = $request->items;
 
         // return $data;
         $order = Order::create($data);
 
-        return response(['order' => $order], 201);
+        collect($data['items'])->map(function($item)use ($order){
+            $foodDrink = Item::where('id', $item)->first();
+            OrderDetail::create([
+                'order_id' => $order->id,
+                'item_id' => $item,
+                'price' => $foodDrink->price
+            ]);
+        });
+
+        return response([
+            'order' => $order,
+            'items' => $data['items']
+        ], 201);
     }
 }
